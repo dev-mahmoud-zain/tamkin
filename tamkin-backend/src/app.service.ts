9d9dad95d48a7dcf52ce3b4b8b169c85d643d1af
Mahmoud Zain <dev.mahmoud.zain@gmail.com>
2026-04-15 22:21:41 +0200

    update translation files and add homepage translations

--- tamkin-backend/src/app.service.ts
+++ tamkin-backend/src/app.service.ts
@@ -1,24 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { Request } from 'express';
+import { ResponseService } from './Common/Services/Response/response.service';
 
 @Injectable()
 export class AppService {
+
+  constructor(private readonly responseService: ResponseService) { }
   main(req: Request) {
 
-    const language = req.headers["accept-language"] || "en";
+    return this.responseService.success({
+      message: req.t("main:message"),
+      data: req.t('main:data', { returnObjects: true })
+    });
 
-    return {
-      organization: (req as any).t('main:organization', { returnObjects: true }),
-      project: (req as any).t('main:project', { returnObjects: true }),
-      about: (req as any).t('main:about', { returnObjects: true }),
-      whyThisProject: (req as any).t('main:whyThisProject', { returnObjects: true }),
-      targetAudience: (req as any).t('main:targetAudience', { returnObjects: true }),
-      operations: (req as any).t('main:operations', { returnObjects: true }),
-      supportTypes: (req as any).t('main:supportTypes', { returnObjects: true }),
-      people: (req as any).t('main:people', { returnObjects: true }),
-      values: (req as any).t('main:values', { returnObjects: true }),
-      transparency: (req as any).t('main:transparency', { returnObjects: true }),
-      contact: (req as any).t('main:contact', { returnObjects: true }),
-    };
   }
 }
